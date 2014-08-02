921175720706a026a982acabc77b4f2fb9c1c060
ezekmoraes <ezekkkiel@outlook.com>
2014-08-02 13:05:30 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
-//
-//  main.m
-//  Prey-iOS
-//
-//  Created by Carlos Yaconi on 29/09/2010.
-//  Copyright 2010 Fork Ltd. All rights reserved.
-//  License: GPLv3
-//  Full license at "/LICENSE"
-//
+/*
+  main.m
+  Prey-iOS
+
+  Created by Carlos Yaconi on 29/09/2010.
+  Copyright 2010 Fork Ltd. All rights reserved.
+  License: GPLv3
+  Full license at "/LICENSE"
+*/
 
 #import <UIKit/UIKit.h>
 #import "PreyAppDelegate.h"
